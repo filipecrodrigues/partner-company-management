b56f23c984472f1d1b04f09270da326abe4a33c3
Filipe <filipe.candido.rodrigues@gmail.com>
2026-01-12 15:46:42 -0300

    feat(empresas): adiciona componente empresa-card

---
+++ frontend/src/app/features/empresas/components/empresa-card/empresa-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+import { Empresa } from '../../models/empresa.model';
+
+@Component({
+  selector: 'app-empresa-card',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatIconModule],
+  templateUrl: './empresa-card.component.html',
+  styleUrls: ['./empresa-card.component.scss'],
+})
+export class EmpresaCardComponent {
+  @Input() empresa!: Empresa;
+
+  @Output() editar = new EventEmitter<number>();
+  @Output() deletar = new EventEmitter<number>();
+
+  onEditar() {
+    this.editar.emit(this.empresa.id);
+  }
+
+  onDeletar() {
+    this.deletar.emit(this.empresa.id);
+  }
+}
